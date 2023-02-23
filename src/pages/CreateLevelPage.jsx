61a9dd27734820e76d20f4574d083286141e548f
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-23 18:57:27 -0500

    add basic form for creating level and preview img

--- src/pages/CreateLevelPage.jsx
+++ src/pages/CreateLevelPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CreateLevelPage() {
+  const [imgFile, setImgFile] = useState(undefined);
+  const [preview, setPreview] = useState(undefined);
+
+  useEffect(() => {
+    if (!imgFile) {
+      setPreview(undefined);
+      return () => {};
+    }
+
+    const imgUrl = URL.createObjectURL(imgFile);
+    setPreview(imgUrl);
+
+    return () => URL.revokeObjectURL(imgUrl);
+  }, [imgFile]);
+
   return (
     <main className="create-level-page">
-      Create level page
+      <form>
+        <label htmlFor="level-title">
+          Title
+          <input type="text" id="level-title" required />
+        </label>
+        <label htmlFor="level-image">
+          Image
+          <input
+            type="file"
+            id="level-image"
+            accept="image/*"
+            multiple={false}
+            required
+            onChange={(e) => setImgFile(e.target.files[0])}
+          />
+        </label>
+      </form>
+      <img src={preview} alt="Preview" />
     </main>
   );
 }
